8fae1931559aae9fd3f40ea900b10b4c4b1ca34b
arcel martial <kontcheu23@gmail.com>
2023-12-12 05:57:17 +0100

    v9

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,7 +13,7 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.profile');
     }
 
     /**
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -59,19 +59,15 @@
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
-                    <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="button.html" class="dropdown-item">Buttons</a>
-                            <a href="typography.html" class="dropdown-item">Typography</a>
-                            <a href="element.html" class="dropdown-item">Other Elements</a>
-                        </div>
-                    </div>
-                    <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-                    <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
-                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
-                    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
+                    <a href="{{ route('dashboard') }}" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+
+                    <a href="{{ route('profile.index') }}" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Profile</a>
+                    <a href="{{ route('service.index') }}" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Services</a>
+                    <a href="{{ route('projet.index') }}" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Projets</a>
+                    <a href="{{ route('blog.index') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>BLogs</a>
+                    <a href="{{ route('contact.index') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Contact</a>
+                    <a href="{{ route('site.index') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Site</a>
+
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
                         <div class="dropdown-menu bg-transparent border-0">
@@ -179,7 +175,7 @@
             </nav>
             <!-- Navbar End -->
 
-         <main>
+         <main class="container-fluid pt-4 px-4">
             {{ $slot }}
          </main>
 
